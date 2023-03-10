1f95ce45147f54f87793d44c96791f9f1d76b0b2
Hiago de Oliveira Mendes <99771272+hiag0liveira@users.noreply.github.com>
2023-03-10 19:31:55 -0300

    Delete should_dsl directory

--- should_dsl/__init__.py
+++
@@ -1,9 +0,0 @@
-from should_dsl.dsl import (should,
-                         should_not,
-                         matcher,
-                         add_predicate_regex,
-                         matcher_configuration,
-                         aliases,
-                         ShouldNotSatisfied)
-from should_dsl import matchers
-
--- should_dsl/backwardscompat.py
+++
@@ -1,6 +0,0 @@
-import sys
-
-if sys.version_info >= (3,):
-    string_types = (str,)
-else:
-    string_types = (basestring,)
